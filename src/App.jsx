207bf4662aaae838d92f9a49968d7e92be83aef6
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-28 15:21:53 +0100

    feat: add font family and products component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import Products from "./components/Products.jsx";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Products />
       <Footer />
     </>
   );
