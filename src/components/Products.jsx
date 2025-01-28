8b0b7769bd70726588ca5b4b258e86b08e8a5923
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-28 17:13:57 +0100

    feat: add products section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,66 @@
 import React from "react";
 
 export default function Products() {
-  return <></>;
+  return (
+    <>
+      <section className="products">
+        <div className="container">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">
+                  <img
+                    className="products--icons"
+                    src="../img/buy-comics-digital-comics.png"
+                    alt="Per comprare fumetti digitali"
+                  />
+                  <p>DIGITAL COMICS</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    className="products--icons"
+                    src="../img/buy-comics-merchandise.png"
+                    alt="Per comprare fumetti digitali"
+                  />
+                  <p>DC MERCHANDISE</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    className="products--icons"
+                    src="../img/buy-comics-subscriptions.png"
+                    alt="Per comprare fumetti digitali"
+                  />
+                  <p>SUBSCRIPTION</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    className="products--icons"
+                    src="../img/buy-comics-shop-locator.png"
+                    alt="Per comprare fumetti digitali"
+                  />
+                  <p>COMIC SHOP LOCATOR</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    className="products--icons"
+                    src="../img/buy-dc-power-visa.svg"
+                    alt="Per comprare fumetti digitali"
+                  />
+                  <p>DC POWER VISA</p>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+    </>
+  );
 }
