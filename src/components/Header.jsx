82e3247342ba7c46f3f8c21212548953840590ce
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-28 16:08:22 +0100

    feat: add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,55 @@
 import React from "react";
 
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <div>
+          <a href="#">
+            <img
+              className="header--logo"
+              src="../img/dc-logo.png"
+              alt="Logo DC Comics"
+            />
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+              <a className="selected" href="#">
+                COMICS
+              </a>
+            </li>
+            <li>
+              <a href="#">MOVIES</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">GAMES</a>
+            </li>
+            <li>
+              <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+              <a href="#">VIDEOS</a>
+            </li>
+            <li>
+              <a href="#">FANS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              <a href="#">SHOP</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
