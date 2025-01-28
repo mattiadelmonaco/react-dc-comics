6ecc562aad115cbd3429d03ac7f2e448186f52fb
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-28 18:02:46 +0100

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,114 @@
 import React from "react";
 
 export default function Footer() {
-  return <></>;
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div>
+            <div className="footer--top">
+              <div>
+                <h4>DC COMICS</h4>
+                <ul>
+                  <li>
+                    <a href="#">Characters</a>
+                  </li>
+                  <li>
+                    <a href="#">Comics</a>
+                  </li>
+                  <li>
+                    <a href="#">Movies</a>
+                  </li>
+                  <li>
+                    <a href="#">TV</a>
+                  </li>
+                  <li>
+                    <a href="#">Games</a>
+                  </li>
+                  <li>
+                    <a href="#">Videos</a>
+                  </li>
+                  <li>
+                    <a href="#">News</a>
+                  </li>
+                </ul>
+                <h4>SHOP</h4>
+                <ul>
+                  <li>
+                    <a href="#">Shop DC</a>
+                  </li>
+                  <li>
+                    <a href="#">Shop DC Collectibles</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4>DC</h4>
+                <ul>
+                  <li>
+                    <a href="#">Terms Of Use</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy policy (New)</a>
+                  </li>
+                  <li>
+                    <a href="#">Ad Choises</a>
+                  </li>
+                  <li>
+                    <a href="#">Advertising</a>
+                  </li>
+                  <li>
+                    <a href="#">Jobs</a>
+                  </li>
+                  <li>
+                    <a href="#">Subscriptions</a>
+                  </li>
+                  <li>
+                    <a href="#">Talent Workshops</a>
+                  </li>
+                  <li>
+                    <a href="#">CPSC Certificates</a>
+                  </li>
+                  <li>
+                    <a href="#">Ratings</a>
+                  </li>
+                  <li>
+                    <a href="#">Shop Help</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4>SITES</h4>
+                <ul>
+                  <li>
+                    <a href="#">DC</a>
+                  </li>
+                  <li>
+                    <a href="#">MAD Magazine</a>
+                  </li>
+                  <li>
+                    <a href="#">DC Kids</a>
+                  </li>
+                  <li>
+                    <a href="#">DC Universe</a>
+                  </li>
+                  <li>
+                    <a href="#">DC Power Visa</a>
+                  </li>
+                </ul>
+              </div>
+              <img
+                className="footer--top--logo"
+                src="../img/dc-logo-bg.png"
+                alt="Logo grande DC Comics"
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
