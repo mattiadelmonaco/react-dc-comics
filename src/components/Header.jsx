fd4f1dcc01cf8e0d4b068c814a3c628510f2485e
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-28 16:50:44 +0100

    feat: add main and fix container header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,51 +4,55 @@ export default function Header() {
   return (
     <>
       <header>
-        <div>
-          <a href="#">
-            <img
-              className="header--logo"
-              src="../img/dc-logo.png"
-              alt="Logo DC Comics"
-            />
-          </a>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">CHARACTERS</a>
-            </li>
-            <li>
-              <a className="selected" href="#">
-                COMICS
+        <div className="container">
+          <div className="header--d-flex">
+            <div>
+              <a href="#">
+                <img
+                  className="header--logo"
+                  src="../img/dc-logo.png"
+                  alt="Logo DC Comics"
+                />
               </a>
-            </li>
-            <li>
-              <a href="#">MOVIES</a>
-            </li>
-            <li>
-              <a href="#">TV</a>
-            </li>
-            <li>
-              <a href="#">GAMES</a>
-            </li>
-            <li>
-              <a href="#">COLLECTIBLES</a>
-            </li>
-            <li>
-              <a href="#">VIDEOS</a>
-            </li>
-            <li>
-              <a href="#">FANS</a>
-            </li>
-            <li>
-              <a href="#">NEWS</a>
-            </li>
-            <li>
-              <a href="#">SHOP</a>
-            </li>
-          </ul>
-        </nav>
+            </div>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                  <a className="selected" href="#">
+                    COMICS
+                  </a>
+                </li>
+                <li>
+                  <a href="#">MOVIES</a>
+                </li>
+                <li>
+                  <a href="#">TV</a>
+                </li>
+                <li>
+                  <a href="#">GAMES</a>
+                </li>
+                <li>
+                  <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                  <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                  <a href="#">FANS</a>
+                </li>
+                <li>
+                  <a href="#">NEWS</a>
+                </li>
+                <li>
+                  <a href="#">SHOP</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </header>
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+const content = "--> Content goes here <--";
+
 export default function Main() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <div className="container">
+          <div className="main--h3">
+            <h3>{content}</h3>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
